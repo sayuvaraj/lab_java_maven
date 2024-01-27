cb49fae3697607415073fdfd1a830843e3d79585
sayuvaraj <s.a.yuvaraj26@gmail.com>
2024-01-27 23:58:17 +0530

    first commit on lab java

---
+++ src/main/java/lab_group_id/lab_java_artifact/App.java
@@ -0,0 +1,13 @@
+package lab_group_id.lab_java_artifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/lab_group_id/lab_java_artifact/AppTest.java
@@ -0,0 +1,20 @@
+package lab_group_id.lab_java_artifact;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
